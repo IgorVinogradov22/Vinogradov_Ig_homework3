2f8fd7fc397801dc6f09ef96277a5ce2e8911ef0
Igor <yagr71@yandex.ru>
2022-04-27 22:10:26 +0400

    Add task1

---
+++ Examples/task1/Program.cs
@@ -0,0 +1,17 @@
+﻿
+void Qwer (int N)
+{
+    int num1 = (N/10000);
+    int num2 = (N /1000) % 10;
+    int num4 = (N /10) % 10;
+    int num5 =  N % 10;
+
+        if ((num1 == num5) && (num2 == num4))
+    {
+        Console.WriteLine("Да");
+    }
+        else Console.WriteLine("Нет");
+}        
+ 
+ int N = 45254;
+ Qwer (N);
